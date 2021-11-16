3d9f92ee012e5fd731aae06b796226958b458371
LarsGehrmann <94363869+LarsGehrmann@users.noreply.github.com>
2021-11-16 16:53:58 +0100

    upwind for part 2 with general Nx
    bro I love you, don't study so much !

--- main2.m
+++ main2.m
@@ -2,36 +2,33 @@ clear all
 close all
 clc
 
+Nx = 1000; % number of spatial grid points 
 v = 1; % velocity of fluid
 alpha = 0.5; % heat exchange parameter
 L = 5; % length of the pipe
 Tcool = 50; % temperature of the cooling
 Thot = 200; % temperature of the hot whatever
-dx = L/100; % spatial steplength
+dx = L/Nx; % spatial steplength
 CFL = 1; % CFL number
 dt = dx*CFL; % time step length
 tend = 6; % maximum time
-uini = zeros(100,1);
+uini = zeros(Nx,1);
 uini(:) = Tcool; % specifying initial conditions
 sol = uini;
 for t = 0:dt:tend
     u0 = boundary(t);
-    sol = upwind2(sol,dt,dx,u0);
+    sol = upwind2(sol,dt,dx,u0,Nx);
 end
 %%%%% plot
 x = linspace(0,5,length(sol(:,1)));
 t = linspace(0,6,length(sol(1,:)));
-plot(t,sol(1,:),'bX')
+plot(t,sol(500,:),'bX')
 grid on
 xlabel('time \rightarrow')
 ylabel('solution \rightarrow')
 legend('solution at x = 0')
 %%%%% end of plot
-t = linspace(0,6,length(sol(1,:)));
-for k=1:length(t)
-u0test(k) = boundary(t(k));
-end
-% function that generates boundary conditions u0 at x = 0
+% function that generates boundary conditions u0(t) at x = 0
 function u0 = boundary(t)
 Tcool = 50;
 Thot = 200;
--- upwind2.m
+++ upwind2.m
@@ -1,4 +1,4 @@
-function solnext = upwind2(sol,dt,dx,u0)
+function solnext = upwind2(sol,dt,dx,u0,Nx)
 lastrow = sol(:,end); %obtaining the last time step to calculate the next time step
 lambda = dt/dx; % CFL number
 v=1; % velocity of the fluid
@@ -6,17 +6,17 @@ alpha = 0.5; % heat exchange parameter, note that alpha =/= alphaj-1 !
 Tcool = 50;
 alphajminus1 = v*lambda; % factor of u_{j-1}^n
 betaj = 1-lambda*v-dt*alpha; %factor of u_{j}^n
-A = zeros(99); % Matrix A that is used to calculate the next timestep
+A = zeros(Nx-1); % Matrix A that is used to calculate the next timestep
 A(1,1) = betaj;
-for k = 2:99
+for k = 2:Nx-1
     A(k,k-1) = alphajminus1;
     A(k,k) = betaj;
 end
 A = sparse(A);
-b = zeros(99,1);
-b(2:99,1) = dt*alpha*Tcool;
+b = zeros(Nx-1,1);
+b(2:Nx-1,1) = dt*alpha*Tcool;
 b(1,1) = alphajminus1*lastrow(1) + dt*alpha*Tcool;
-u_next_timestep_2_100 = A * lastrow(2:100)+b;
-u_next_timestep = [u0;u_next_timestep_2_100];
+u_next_timestep_2_Nx = A * lastrow(2:Nx)+b;
+u_next_timestep = [u0;u_next_timestep_2_Nx];
 solnext = [sol,u_next_timestep];
 end
