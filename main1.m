da16bfaac2b0f9e627d4b58f858feb4ffc9cf1c5
LarsGehrmann <94363869+LarsGehrmann@users.noreply.github.com>
2021-11-16 12:41:22 +0100

    Did the upwind scheme

--- main1.m
+++ main1.m
@@ -6,17 +6,24 @@ D = 4.5;
 a = 1;
 tau = 2.2;
 dx = D/100;
-CFL = 1;
+CFL = 0.1;
 dt = dx*CFL;
-tend
+tend = 6;
 uini = zeros(100,1);
 sol = uini;
 
 for t = 0:dt:tend
-    u0 = gsin(t,tau);
+    u0 = gsq(t,tau);
     sol = upwind(sol,dt,dx,u0);
 end
-
+%%%%% plot
+x = linspace(0,D,100);
+plot(x,sol(:,end),'bX')
+grid on
+xlabel('x \rightarrow')
+ylabel('solution \rightarrow')
+legend('solution at t = 6')
+%%%%% end of plot
 %Initial condition functions
 function u0 = gsin(t, tau)
     u0 = sin((t/tau)*2*pi);
--- upwind.m
+++ upwind.m
@@ -1,4 +1,19 @@
-function solnext = upwind(sol,dt,dx,u0)
-    solnext = sol;
-    %hi there
+function solnext = up(sol,dt,dx,u0)
+lastrow = sol(:,end); %obtaining the last time step to calculate the next time step 
+lambda = dt/dx; % CFL number
+a=1; % transport velocity
+alpha = a*dt/dx; % factor of u_{j-1}^n
+beta = 1-a*dt/dx; %factor of u_{j}^n
+A = zeros(99); % Matrix A that is used to calculate the next timestep
+A(1,1) = beta;
+for k = 2:99
+    A(k,k-1) = alpha;
+    A(k,k) = beta;
+end
+A = sparse(A);
+b=zeros(99,1);
+b(1) = alpha * lastrow(1);
+u_next_timestep_2_100 = A * lastrow(2:100)+b;
+u_next_timestep = [u0;u_next_timestep_2_100];
+solnext = [sol,u_next_timestep];
 end
