074fc745dc2df580ed7bc132c2b6bcd5796f302f
Bastiaan van Hoorn <bastiaan97.bvh@gmail.com>
2021-11-17 12:31:57 +0100

    Implemented Lax Friedrich for part 1

--- extrapolate.m
+++ extrapolate.m
@@ -1,3 +1,3 @@
-function uNn = extrapolate(UNm1, UNm2)
-
+function uN = extrapolate(uNm1, uNm2)
+    uN =2*uNm1 - uNm2;
 end
--- main1.m
+++ main1.m
@@ -5,20 +5,20 @@ clc
 D = 4.5;
 a = 1;
 tau = 2.2;
-Nx = 1000; % number of grid points
-dx = D/Nx;
+Nx = 100; % number of grid points
+dx = D/(Nx-1);
 CFL = 1;
 dt = dx*CFL;
 tend = 6;
 uini = zeros(Nx,1);
 sol = uini;
 for t = 0:dt:tend
-    u0 = gsin(t,tau);
-    sol = upwind(sol,dt,dx,u0,Nx);
+    u0 = gsq(t,tau);
+    sol = laxfriedrich(sol,dt,dx,u0,Nx);
 end
 %%%%% plot
 x = linspace(0,D,Nx);
-plot(x,sol(:,end),'bX')
+plot(x,sol(:,end))
 grid on
 xlabel('x \rightarrow')
 ylabel('solution \rightarrow')
