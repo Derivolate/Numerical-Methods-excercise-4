6d1b0c07a454ce84000f729031c91ebecdbd1d78
LarsGehrmann <94363869+LarsGehrmann@users.noreply.github.com>
2021-11-17 12:26:38 +0100

    corrected dx and implemented 3d plot

--- main2.m
+++ main2.m
@@ -1,6 +1,9 @@
 clear all
 close all
 clc
+set(0,'defaultTextInterpreter','latex');
+set(0, 'defaultLegendInterpreter','latex');
+set(0, 'defaultAxesTickLabelInterpreter','latex');
 
 Nx = 1000; % number of spatial grid points 
 v = 1; % velocity of fluid
@@ -8,14 +11,14 @@ alpha = 0.5; % heat exchange parameter
 L = 5; % length of the pipe
 Tcool = 50; % temperature of the cooling
 Thot = 200; % temperature of the hot whatever
-dx = L/Nx; % spatial steplength
+dx = L/(Nx-1); % spatial steplength
 CFL = 1; % CFL number
 dt = dx*CFL; % time step length
 tend = 6; % maximum time
 uini = zeros(Nx,1);
 uini(:) = Tcool; % specifying initial conditions
 sol = uini;
-%%upwind
+%%%%%%%%% upwind
 lambda = dt/dx; % CFL number
 alphajminus1 = v*lambda; % factor of u_{j-1}^n
 betaj = 1-lambda*v-dt*alpha; %factor of u_{j}^n
@@ -30,14 +33,16 @@ for t = 0:dt:tend
     u0 = boundary(t);
     sol = upwind2(sol,dt,dx,u0,Nx,A);
 end
+%%%%%%%%%
 %%%%% plot
 x = linspace(0,5,length(sol(:,1)));
 t = linspace(0,6,length(sol(1,:)));
-plot(t,sol(500,:),'bX')
+[X,T] = meshgrid(t,x);
+mesh(X,T,sol)
 grid on
-xlabel('time \rightarrow')
-ylabel('solution \rightarrow')
-legend('solution at x = 0')
+xlabel('time')
+ylabel('x')
+zlabel('Temperature')
 %%%%% end of plot
 % function that generates boundary conditions u0(t) at x = 0
 function u0 = boundary(t)
